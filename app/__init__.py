23f23c1ee1c52590c4a25a0295e35d31dc35f680
Mary Waithera <njihiamary11@gmail.com>
2020-05-11 11:26:50 +0300

    Create initial file structure

---
+++ app/__init__.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_mail import Mail
+from flask_wtf.csrf import CSRFProtect
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_simplemde import SimpleMDE
+
+simplemde = SimpleMDE()
+
+photos = UploadSet('photos',IMAGES)
+
+csrf = CSRFProtect()
+
+mail=Mail()
+
+login_manager = LoginManager()
+login_manager.sesssion_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+db=SQLAlchemy()
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+
+    '''
+    function facilitates app and extension initialization, blueprint registration, and app configurations creation
+    '''
+
+    #create flask instance
+    app = Flask(__name__)
+
+    #configurations creation
+    app.config.from_object(config_options[config_name])
+
+    #configure UploadSet
+    configure_uploads(app,photos)
+
+    #extensions initialization
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    csrf.init_app(app)
+    
+
+    #blueprint registration
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    return app
---
+++ config.py
@@ -0,0 +1,47 @@
+import os
+
+class Config:
+
+    '''
+    class facilitates the creation of app configurations
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    
+
+class DevConfig(Config):
+
+    '''
+    class inherits general configurations from Config class
+    '''
+
+    DEBUG = True
+
+class TestConfig(Config):
+
+    '''
+    class inherits general configurations from config class
+    '''
+    pass
+
+class ProdConfig(Config):
+
+    '''
+    class inherits general configurations from config class
+    '''
+    pass
+
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
