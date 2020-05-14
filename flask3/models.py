0b0d7276c579ff47c118c5e01f425b0b7aa9955f
dennismwaniki67 <dennismwaniki67@gmail.com>
2020-05-14 15:42:10 +0300

    added some features

--- flask3/models.py
+++ flask3/models.py
@@ -1,4 +1,3 @@
-
 from datetime import datetime
 from flask3 import db, login_manager
 from flask_login import UserMixin
@@ -18,12 +17,14 @@ class User(db.Model,UserMixin):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
 
 
     def __repr__(self):
@@ -35,9 +36,13 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
+    
+    title = db.Column(db.Text,  nullable=True)
+    
     def __repr__(self):
         return f"Comment('{self.content}', '{self.date_posted}')"
+
 class DementiaNews():
 
     '''
