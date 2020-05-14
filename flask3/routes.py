dd063fbeaa65e12a4c26ab818e966c077bc1572e
Mary Waithera <njihiamary11@gmail.com>
2020-05-14 17:41:44 +0300

    resolve conflicts and add posts link in navbar

--- flask3/routes.py
+++ flask3/routes.py
@@ -53,7 +53,7 @@ def selfcare():
 @app.route('/home')
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=6)
    
     return render_template('home.html',posts= posts)
 
