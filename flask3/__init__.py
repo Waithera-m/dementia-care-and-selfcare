5cf6ec72c9609d054be38e2cff84753855972a20
Mary Waithera <njihiamary11@gmail.com>
2020-05-15 14:56:22 +0300

    change database path to facilitate deployment on heroku

--- flask3/__init__.py
+++ flask3/__init__.py
@@ -8,7 +8,7 @@ from flask_login import LoginManager
 app = Flask(__name__) 
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'c26d6bde3e77e96c634e8a88fd857560'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://mary:pixie01@localhost/dementia'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'DATABASE_URL'
 db = SQLAlchemy(app)
 bcrypt =Bcrypt(app)
 login_manager = LoginManager(app)
